80b8f28315c056e8d66708e922a69140be356793
westofsky <westofsky159@gmail.com>
2020-11-21 05:38:40 +0000

    Add routing

---
+++ client/src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="Home">
+	여긴 홈
+	<router-link to="/">Go to Main</router-link>
+  </div>
+</template>
+<script>
+export default {
+	name: 'Home',
+}
+</script>
---
+++ client/src/views/Main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="Main">
+	여긴 메인
+	<router-link to="/home">Go to home</router-link>
+  </div>
+</template>
+<script>
+export default {
+	name: 'Main',
+}
+</script>
+<style scoped>
+	.Main{
+		width : 100%;
+		height : 100%;
+		background-color : yellow;
+	}
+</style>

85c2c40374da751be0f7e0be862c090146a0cdba
westofsky <westofsky159@gmail.com>
2020-11-29 06:27:07 +0000

    Add SCSS

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -1,14 +1,17 @@
 <template>
 	<div class="Login">
 		<div class="wrap_form">
-			<p>Login</p>
+			<p>로그인 페이지</p>
 			<div class="login-input-container">
-				<b-form-group class="login-input-id" label="ID">
+				<b-form-group class="login-input-id" label="이메일">
 					<b-form-input v-model="user.input_id" type="text" placeholder="Your ID" />
 				</b-form-group>
-				<b-form-group class="login-input-pw" label="PASSWORD">
+				<b-form-group class="login-input-pw" label="비밀번호">
 					<b-form-input v-model="user.input_pw" type="text" placeholder="Your Password" />
 				</b-form-group>
+				<div class="login-input-btn">
+					<b-button variant="outline-primary">로그인</b-button>
+				</div>
 			</div>
 			<router-link to="/">Go to home</router-link>
 			<router-link to="/register">Go to register</router-link>
@@ -28,7 +31,7 @@
 		},
 	};
 </script>
-<style>
+<style scoped lang="scss">
 	.wrap_form p {
 		font-size: 30px;
 	}
@@ -36,5 +39,8 @@
 		width: 70%;
 		text-align: left;
 		margin: 0 auto;
+		.login-input-btn{
+			text-align : center;
+		}
 	}
 </style>
